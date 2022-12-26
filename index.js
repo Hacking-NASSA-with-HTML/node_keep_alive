f962dbe4fd733bdb4b2309ec1686399d373e855b
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2022-12-27 01:10:16 +0400

    feat: prepare to deploy

--- index.js
+++ index.js
@@ -27,8 +27,8 @@ const interval = setInterval(async () => {
     try {
         const response = await fetch('https://render-back-end-nikky-pedia.onrender.com/')
         // const body = await response.text();
-        // console.log(response.ok)
-        // console.log(response.status)
+        console.log(response.ok)
+        console.log(response.status)
         // console.log(body)
     } catch (error) {
         console.log(error)
